f3e7e82076d452fa7f8eb59fee7d2eb486d4c7c4
mylenavendramini <myvendramini@gmail.com>
2022-12-24 08:42:38 +0000

    server types user.ts

--- server/src/types/user.ts
+++ server/src/types/user.ts
@@ -1,3 +1,9 @@
 export interface IUser {
+  userName: string;
+  email: string;
   password: string;
+  comparePassword: (
+    candidatePassword: string,
+    cb: (err: any, isMatch: any) => void
+  ) => void;
 }
