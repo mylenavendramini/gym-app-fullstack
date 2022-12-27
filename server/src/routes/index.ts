0c09777d2a3a0a5835a78122b5153ebf6a3600a8
mylenavendramini <myvendramini@gmail.com>
2022-12-27 08:43:54 +0000

    server routes index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,12 +5,27 @@ import {
   getWorkouts,
   updateWorkout,
 } from "../controllers/workouts/index";
+import {
+  getLogin,
+  postLogin,
+  logout,
+  getSignup,
+  postSignup,
+} from "../controllers/auth/auth";
+import { ensureAuth, ensureGuest } from "../middleware/auth";
 
 const router: Router = Router();
 
-router.get("/", getWorkouts);
+router.get("/", ensureAuth, getWorkouts);
 router.post("/create-workout", createWorkout);
 router.put("/update-workout/:id", updateWorkout);
 router.delete("/delete-workout/:id", deleteWorkout);
+
+// Auth:
+router.get("/login", getLogin);
+router.post("/login", postLogin);
+router.get("/logout", logout);
+router.get("/signup", getSignup);
+router.post("/signup", postSignup);
 
 export default router;
