8b640a0cf91302bb26ff0f621b75e1aab9516075
mylenavendramini <myvendramini@gmail.com>
2022-12-30 08:46:55 +0000

    server controllers auth

---
+++ server/src/controllers/auth/auth.ts
@@ -0,0 +1,143 @@
+import passport from "passport";
+import validator from "validator";
+import { Request, Response, NextFunction } from "express";
+import User from "../../models/User";
+import { IUser } from "../../types/user";
+// import { Session } from "express-session";
+
+interface SessionData {
+  returnTo?: string;
+}
+
+declare global {
+  namespace Express {
+    interface Session {
+      returnTo?: string;
+    }
+  }
+}
+
+export const getLogin = (req: Request, res: Response) => {
+  if (req.user) {
+    return res.redirect("/profile");
+  }
+  res.render("login", {
+    title: "Login",
+  });
+};
+
+export const postLogin = (
+  req: Request & { session: SessionData },
+  res: Response,
+  next: NextFunction
+) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (validator.isEmpty(req.body.password))
+    validationErrors.push({ msg: "Password cannot be blank." });
+
+  if (validationErrors.length) {
+    const errorMessages = validationErrors.map((error) => error.msg);
+    req.flash("errors", errorMessages);
+    return res.redirect("/login");
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+
+  passport.authenticate("local", (err, user: IUser, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      req.flash("errors", info);
+      return res.redirect("/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      //   req.flash("success", { msg: "Success! You are logged in." });
+      req.flash("success", "Success! You are logged in.");
+      res.redirect(req.session.returnTo || "/profile");
+    });
+  })(req, res, next);
+};
+
+export const logout = (req: Request, res: Response) => {
+  req.logout(() => {
+    console.log("User has logged out.");
+  });
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    // req.user = null;
+    req.user = undefined;
+    res.redirect("/");
+  });
+};
+
+export const getSignup = (req: Request, res: Response) => {
+  if (req.user) {
+    return res.redirect("/profile");
+  }
+  res.render("signup", {
+    title: "Create Account",
+  });
+};
+
+export const postSignup = (req: Request, res: Response, next: NextFunction) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (!validator.isLength(req.body.password, { min: 8 }))
+    validationErrors.push({
+      msg: "Password must be at least 8 characters long",
+    });
+  if (req.body.password !== req.body.confirmPassword)
+    validationErrors.push({ msg: "Passwords do not match" });
+
+  if (validationErrors.length) {
+    const errorMessages = validationErrors.map((error) => error.msg);
+    req.flash("errors", errorMessages);
+    return res.redirect("../signup");
+  }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  User.findOne(
+    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    (err: any, existingUser: any) => {
+      if (err) {
+        return next(err);
+      }
+      if (existingUser) {
+        req.flash(
+          "errors",
+          "Account with that email address or username already exists."
+        );
+        return res.redirect("../signup");
+      }
+      user.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/profile");
+        });
+      });
+    }
+  );
+};
